105266610a1451bb870739329938f245b18da0c1
VINAYAK BENI <benivinayak@gmail.com>
2025-07-06 21:20:35 +0530

    03:trpc setup

--- vibe/src/app/page.tsx
+++ vibe/src/app/page.tsx
@@ -1,5 +1,12 @@
+'use client';
+import { useTRPC } from "@/trpc/client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { QueryClient } from "@tanstack/react-query";
+import { tr } from "date-fns/locale";
 
 const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "world" }));
   
   return (<div>
     Hello world
